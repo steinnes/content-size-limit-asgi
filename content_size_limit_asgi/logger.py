70dfc0208506554a6656d9ec2ca9e6920315b951
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2019-12-20 14:40:31 +0000

    black: painting the project black

--- content_size_limit_asgi/logger.py
+++ content_size_limit_asgi/logger.py
@@ -6,7 +6,7 @@ def setup_logging(logger, level=None):
         level = logging.INFO
 
     handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(levelname)s %(asctime)s %(name)s: %(message)s')
+    formatter = logging.Formatter("%(levelname)s %(asctime)s %(name)s: %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(level)
